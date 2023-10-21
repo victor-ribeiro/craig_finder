8153661cba81d78b92ec8dcf76fba4cf5e3dcc7f
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2023-10-20 22:04:45 -0300

    model 0.1

--- model/decision_tree.py
+++ model/decision_tree.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer
 from sklearn.metrics import classification_report, log_loss
 from sliceline.slicefinder import Slicefinder
 
@@ -14,11 +15,11 @@ from sliceline.slicefinder import Slicefinder
 # %%
 target_col = "LeaveOrNot"
 attr_col = [
-    "intercept",
     "Education",
     "JoiningYear",
-    "City_0",
-    "City_1",
+    "City_Bangalore",
+    "City_New",
+    "Delhi,City_Pune",
     "PaymentTier",
     "Age",
     "ExperienceInCurrentDomain",
@@ -28,12 +29,16 @@ attr_col = [
     "EverBenched_Yes",
 ]
 
-data = pd.read_csv("../data/employee/t_Employee.csv", index_col="Unnamed: 0")
+data = pd.read_csv("../data/employee/t_Employee.csv")
+scaler = Normalizer()
+
+# data = pd.DataFrame(data=scaler.fit_transform(data.values), columns=data.columns)
+
 d = pd.read_csv(
     "../data/employee/Employee.csv",
 )
 train_data, test_data = train_test_split(
-    data, random_state=42, shuffle=True, train_size=0.7
+    data, random_state=42, shuffle=True, train_size=0.8
 )
 # %%
 #
@@ -43,11 +48,11 @@ train_data, test_data = train_test_split(
 y_data = train_data[target_col].values
 x_data = train_data.drop(target_col, axis="columns").values
 
-dt = DecisionTreeClassifier(max_depth=5)
-dt.fit(x_data, y_data)
+dt = DecisionTreeClassifier()
+dt.fit(scaler.fit_transform(x_data), y_data)
 
 importance = dt.feature_importances_
-
+print(len(attr_col), len(importance))
 plt.bar(attr_col, importance)
 plt.xticks(range(0, len(attr_col)), attr_col, rotation=90)
 plt.show()
@@ -56,7 +61,7 @@ plt.show()
 y_data = test_data[target_col].values
 x_data = test_data.drop(target_col, axis="columns").values
 
-y_pred = dt.predict(x_data)
+y_pred = dt.predict(scaler.fit_transform(x_data))
 print(classification_report(y_true=y_data, y_pred=y_pred))
 
 # %%
@@ -77,10 +82,10 @@ prob_n = np.clip(y_prob[..., 1], eps, 1 - eps)
 erro_n = logLoss(y=y_data.values, y_prob=prob_n)
 
 slice_finder = Slicefinder(
-    alpha=0.95, k=5, max_l=x_data.shape[1], min_sup=1, verbose=False
+    alpha=0.95, k=5, max_l=x_data.shape[1], min_sup=1, verbose=True
 )
 
-slice_finder.fit(X=x_data, errors=erro_n)
+slice_finder.fit(X=d.iloc[x_data.index], errors=erro_n)
 pd.DataFrame(
     slice_finder.top_slices_,
     columns=slice_finder.feature_names_in_,
@@ -93,10 +98,10 @@ pd.DataFrame(
 #
 
 attr_col = [
-    "intercept",
+    "City_Bangalore",
+    "City_New",
+    "Delhi,City_Pune",
     "Education",
-    "City_0",
-    "City_1",
     "PaymentTier",
     "Age",
     "ExperienceInCurrentDomain",
@@ -111,7 +116,7 @@ y_data = train_data[target_col].values
 x_data = train_data.drop([target_col, "JoiningYear"], axis="columns").values
 
 dt = DecisionTreeClassifier()
-dt.fit(x_data, y_data)
+dt.fit(scaler.fit_transform(x_data), y_data)
 
 importance = dt.feature_importances_
 
@@ -123,7 +128,7 @@ plt.show()
 y_data = test_data[target_col].values
 x_data = test_data.drop([target_col, "JoiningYear"], axis="columns").values
 
-y_pred = dt.predict(x_data)
+y_pred = dt.predict(scaler.fit_transform(x_data))
 print(classification_report(y_true=y_data, y_pred=y_pred))
 
 
@@ -140,14 +145,14 @@ def logLoss(y, y_prob):
 y_prob = dt.predict_proba(x_data.values)
 eps = 10e-15
 
-prob_n = np.clip(y_prob[..., 0], eps, 1 - eps)
+prob_n = np.clip(y_prob[..., 1], eps, 1 - eps)
 erro_n = logLoss(y=y_data.values, y_prob=prob_n)
 
 slice_finder = Slicefinder(
     alpha=0.95, k=5, max_l=x_data.shape[1], min_sup=1, verbose=False
 )
 
-slice_finder.fit(X=x_data, errors=erro_n)
+slice_finder.fit(X=d.iloc[x_data.index], errors=erro_n)
 pd.DataFrame(
     slice_finder.top_slices_,
     columns=slice_finder.feature_names_in_,
@@ -158,7 +163,7 @@ d["PaymentTier"].plot(kind="hist")
 plt.show()
 # %%
 
-d.loc[d["PaymentTier"] == 2].groupby("Gender").count()["LeaveOrNot"]
+d.loc[d["Education"] == "Bachelors"].groupby("Gender").count()["LeaveOrNot"]
 # %%
 d.groupby("Gender").count()["LeaveOrNot"]
 # %%
